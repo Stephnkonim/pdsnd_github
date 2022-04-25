aaa497756b911a3b6590be46a1190fbc4cbfcf3a
Stephenie-nkonim <nkonimsteph@gmail.com>
2022-04-25 14:11:31 +0100

    Add comment for creating city-data dictionary comment describes what the key and values in the dictionary should be

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# Create a dictionary with the city names as keys and csv files as values
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
